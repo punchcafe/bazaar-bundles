f97884231dba8161a305b29727f2390d688bb66d
Luca Fruzza <lfruzza@hotmail.co.uk>
2025-12-18 22:03:01 +0000

    task: remove debug endpoint for products and add docs to products service

--- src/main/java/com/example/codingexercise/products/ProductService.java
+++ src/main/java/com/example/codingexercise/products/ProductService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * The product service manages read access to Product information.
+ */
 @Component
 public class ProductService {
     final ProductsCache productsCache;
@@ -13,11 +16,14 @@ public class ProductService {
         this.productsCache = cache;
     }
 
+    /**
+     * Look up a given product by its ID.
+     * If no product exists, returns empty.
+     *
+     * @param productId the product ID
+     * @return the product, if it exists.
+     */
     public Optional<Product> lookup(final String productId) {
         return Optional.ofNullable(this.productsCache.getCache().get(productId));
-    }
-
-    public List<Product> all() {
-        return this.productsCache.getCache().values().stream().toList();
     }
 }
